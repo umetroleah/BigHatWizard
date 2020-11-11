2e7156da047798b69e48c8d2ae76801e172944e9
Michael <wmnieporte@gmail.com>
2020-11-10 20:38:34 -0500

    Add explosion particles and move prefabs

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public ParticleSystem particleSystem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Instantiate(particleSystem, rb.transform.position, rb.transform.rotation);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
