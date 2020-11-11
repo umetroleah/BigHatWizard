afeaa42d3b5120a3ae65821184ae6760ab02e59b
Michael <wmnieporte@gmail.com>
2020-11-10 20:18:21 -0500

    Add in trails from player attack

--- Assets/Scripts/Shot.cs
+++ Assets/Scripts/Shot.cs
@@ -9,20 +9,27 @@ public class Shot : MonoBehaviour
     public Rigidbody2D rb;
     public int damage = 20;
     public GameObject impactEffect;
+    public GameObject trailPrefab;
 
     // Start is called before the first frame update
     void Start()
     {
         rb.velocity = transform.right * speed;
+        Instantiate(trailPrefab, rb.transform.position, rb.transform.rotation);
         //SoundManager.PlaySound("shooting");
     }
 
     void OnTriggerEnter2D(Collider2D hitInfo)
     {
-        GameObject impact = Instantiate(impactEffect, transform.position, transform.rotation, impactEffect.transform.parent);
-        impact.transform.localScale = new Vector2(transform.localScale.x, transform.localScale.y);
-        SoundManager.PlaySound("shot explode");
-        Destroy(gameObject);
+        Trail trail = hitInfo.GetComponent<Trail>();
+        Weapon weapon = hitInfo.GetComponent<Weapon>();
+        if (trail == null && weapon == null)
+        {
+            GameObject impact = Instantiate(impactEffect, transform.position, transform.rotation, impactEffect.transform.parent);
+            impact.transform.localScale = new Vector2(transform.localScale.x, transform.localScale.y);
+            SoundManager.PlaySound("shot explode");
+            Destroy(gameObject);
+        }
     }
 
 }
---
+++ Assets/Scripts/Trail.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trail : MonoBehaviour
+{
+
+    public float speed = 20f;
+    public Rigidbody2D rb;
+    public ParticleSystem particleSystem;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Shot shot = hitInfo.GetComponent<Shot>();
+        Weapon weapon = hitInfo.GetComponent<Weapon>();
+        Trail trail = hitInfo.GetComponent<Trail>();
+        if (shot == null && weapon == null && trail == null)
+        {
+            particleSystem.Stop(true, ParticleSystemStopBehavior.StopEmitting);
+            rb.velocity = transform.right * 0;
+        }
+    }
+}
