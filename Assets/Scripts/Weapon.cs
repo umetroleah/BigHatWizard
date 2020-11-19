abdf09a1f2f04192e19db868bfa6b5f5fd6b3c56
Michael <wmnieporte@gmail.com>
2020-11-19 17:04:59 -0500

    Neon, and combat update

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -5,62 +5,129 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     public Transform firePointForward;
+    public Transform firePointForwardUp;
+    public Transform firePointForwardDown;
     public Transform firePointUp;
     public Transform firePointDown;
     private Rigidbody2D m_Rigidbody2D;
 
     public CharacterController2D controller;
     //replace with array of bullet prefabs dependent on weapon equipped later
-    public GameObject bulletPrefab;
+    public GameObject normalShot;
+    public GameObject powerShot;
 
-    public float cooldown = 0;
-    private float nextShotTime;
-    public float shotKnockback = 1f;
+
+    public float normalCooldown = 0;
+    public float powerCooldown = 0;
+    private float nextNormalShot;
+    private float nextPowerShot;
+    public float normalShotKnockback = 1f;
+    public float powerShotKnockback = 2f;
 
     // Update is called once per frame
 
     void Start()
     {
-        nextShotTime = Time.time;
+        nextNormalShot = Time.time;
+        nextPowerShot = Time.time;
         m_Rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        if (Input.GetButtonDown("Fire1") && Time.time >= nextShotTime)
+        if (Input.GetButtonDown("Fire1") && Time.time >= nextNormalShot)
         {
-            Shoot();
-            nextShotTime = Time.time + cooldown;
+            Shoot(normalShot, true);
+            nextNormalShot = Time.time + normalCooldown;
+        }
+
+        if (Input.GetButtonDown("Fire2") && Time.time >= nextPowerShot)
+        {
+            Shoot(powerShot, false);
+            nextPowerShot = Time.time + powerCooldown;
+            nextNormalShot = Time.time + normalCooldown;
         }
     }
 
-    void Shoot()
+    void Shoot(GameObject shot, bool normal)
     {
         float direction = Input.GetAxisRaw("Vertical");
+        bool angle = Input.GetButton("Angle");
         bool grounded = controller.GroundCheck();
 
-        if (direction > 0)
+        float shotKnockback = 0f;
+        if (normal)
         {
-            //Shooting up
-            Instantiate(bulletPrefab, firePointUp.position, firePointUp.rotation);
-            m_Rigidbody2D.AddForce(new Vector2(0, -4) * shotKnockback * m_Rigidbody2D.mass, ForceMode2D.Impulse);
-        }
-        else if (direction < 0 && !grounded)
-        {
-            //Shooting down from air
-            Instantiate(bulletPrefab, firePointDown.position, firePointDown.rotation);
-            m_Rigidbody2D.AddForce(new Vector2(0, 4) * shotKnockback * m_Rigidbody2D.mass, ForceMode2D.Impulse);
+            shotKnockback = normalShotKnockback;
         }
         else
         {
+            shotKnockback = powerShotKnockback;
+        }
+
+        if (direction > 0)
+        {
+            if(angle)
+            {
+                //Shooting up angle
+                Instantiate(shot, firePointForwardUp.position, firePointForwardUp.rotation);
+                m_Rigidbody2D.AddForce(new Vector2(-2, -2) * shotKnockback * m_Rigidbody2D.mass, ForceMode2D.Impulse);
+            }
+            else
+            {
+                //Shooting up
+                Instantiate(shot, firePointUp.position, firePointUp.rotation);
+                m_Rigidbody2D.AddForce(new Vector2(0, -4) * shotKnockback * m_Rigidbody2D.mass, ForceMode2D.Impulse);
+            }
+        }
+        else if(direction < 0 && !grounded)
+        {
+            if (angle)
+            {
+                //Shooting down angle
+                Instantiate(shot, firePointForwardDown.position, firePointForwardDown.rotation);
+                m_Rigidbody2D.AddForce(new Vector2(2, 2) * shotKnockback * m_Rigidbody2D.mass, ForceMode2D.Impulse);
+            }
+            else
+            {
+                //Shooting down from air
+                Instantiate(shot, firePointDown.position, firePointDown.rotation);
+                m_Rigidbody2D.AddForce(new Vector2(0, 4) * shotKnockback * m_Rigidbody2D.mass, ForceMode2D.Impulse);
+            }
+        }else
+        {
             //Shooting forward
-            Instantiate(bulletPrefab, firePointForward.position, firePointForward.rotation);
-            if(controller.GetDirection())
+            Instantiate(shot, firePointForward.position, firePointForward.rotation);
+            if (controller.GetDirection())
                 m_Rigidbody2D.AddForce(new Vector2(-4, 0) * shotKnockback * m_Rigidbody2D.mass, ForceMode2D.Impulse);
             else
                 m_Rigidbody2D.AddForce(new Vector2(4, 0) * shotKnockback * m_Rigidbody2D.mass, ForceMode2D.Impulse);
         }
 
-        SoundManager.PlaySound("shooting");
+            /*
+            if (direction > 0)
+            {
+                //Shooting up
+                Instantiate(shot, firePointUp.position, firePointUp.rotation);
+                m_Rigidbody2D.AddForce(new Vector2(0, -4) * shotKnockback * m_Rigidbody2D.mass, ForceMode2D.Impulse);
+            }
+            else if (direction < 0 && !grounded)
+            {
+                //Shooting down from air
+                Instantiate(shot, firePointDown.position, firePointDown.rotation);
+                m_Rigidbody2D.AddForce(new Vector2(0, 4) * shotKnockback * m_Rigidbody2D.mass, ForceMode2D.Impulse);
+            }
+            else
+            {
+                //Shooting forward
+                Instantiate(shot, firePointForward.position, firePointForward.rotation);
+                if(controller.GetDirection())
+                    m_Rigidbody2D.AddForce(new Vector2(-4, 0) * shotKnockback * m_Rigidbody2D.mass, ForceMode2D.Impulse);
+                else
+                    m_Rigidbody2D.AddForce(new Vector2(4, 0) * shotKnockback * m_Rigidbody2D.mass, ForceMode2D.Impulse);
+            }
+            */
+
+            SoundManager.PlaySound("shooting");
     }
 }
